5ce7b013ff2004f58ca944e16981582fa7477845
jessicahui0515 <jessicahui0515@gmail.com>
2026-02-08 00:44:12 +0800

    4rd commit

--- src/main/java/com/FSSE2510_project_jessica/FSSE2510_project/security/SecurityConfig.java
+++ src/main/java/com/FSSE2510_project_jessica/FSSE2510_project/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,12 +14,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                     .requestMatchers("/public/**").permitAll()
                     .anyRequest().authenticated()
                 )
-                .cors(cors->Customizer.withDefaults())
+                .cors(Customizer.withDefaults())
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(Customizer.withDefaults())
                 );
